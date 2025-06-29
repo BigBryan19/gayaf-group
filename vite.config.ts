539fe80538e8275c31fb390e89a7a9ede6afce36
BigBryan19 <samaemmanuel2004@gmail.com>
2025-06-29 02:04:01 +0000

    Modified

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gayaf-group/", // 👈 This is critical for correct asset paths
+  // base: "/gayaf-group/", // 👈 This is critical for correct asset paths
 });
